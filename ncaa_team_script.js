0a657a57228f1c3126ec8678889ede2a445cdb61
olivierqc1 <olivierturgeon@hotmail.fr>
2026-02-24 14:29:34 +0100

    Actualizar ncaa_team_script.js

--- ncaa_team_script.js
+++ ncaa_team_script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://ncaa-team-betting.onrender.com'; // ← Mettre à jour avec ton URL Render
+const API_URL = 'https://ncaa-basket.onrender.com/'; // ← Mettre à jour avec ton URL Render
 
 // ============================================================================
 // SCAN MATCHS DU JOUR
